9d9badb973ea9260022465563d457eb1e22bfcd0
hallelujah1019 <1106661258@qq.com>
2020-03-27 18:28:18 +0800

    主页折叠效果和样式细节优化

--- src/components/home-header.vue
+++ src/components/home-header.vue
@@ -1,19 +1,20 @@
 <template>
   <!-- 头部组件 el-row布局 -->
-  <el-row type='flex' justify="space-between" align="middle">
+  <el-row type="flex" justify="space-between" align="middle">
     <!-- 左侧 -->
-    <el-col :span="6" class='left'>
+    <el-col :span="6" class="left">
       <!-- 左侧 -->
-      <i class='el-icon-s-fold'></i>
+      <i :class="{'el-icon-s-fold ':foldSwitch , 'el-icon-s-unfold':unfoldSwitch}" @click="fold"></i>
       <span>世界因为你而美丽</span>
     </el-col>
     <!-- 右侧 -->
-    <el-col :span="3" class='right'>
+    <el-col :span="3" class="right">
       <!-- 头像 -->
-      <img width="50" src="../assets/img/jlq.png">
+      <img width="50" src="../assets/img/jlq.png" />
       <!-- 下拉菜单 -->
-      <el-dropdown trigger="click">
-        <span>你的小琳吖</span>
+      <span>你的小琳吖</span>
+      <el-dropdown trigger="click" placement="bottom-start">
+        <i class="el-icon-s-tools" ></i>
         <el-dropdown-menu slot="dropdown">
           <el-dropdown-item>账户信息</el-dropdown-item>
           <el-dropdown-item>git地址</el-dropdown-item>
@@ -24,10 +25,30 @@
   </el-row>
 </template>
 <script>
-
 export default {
+  data () {
+    return {
+      // 展开开关
+      foldSwitch: true,
+      // 关闭开关
+      unfoldSwitch: false
+    }
+  },
+  methods: {
+    fold () {
+      // 调用父组件（home.vue）的折叠方法
+      this.$emit('foldMetod')
+      // 折叠图标和展开图标之间的切换
+      if (this.foldSwitch === true) {
+        this.foldSwitch = false
+        this.unfoldSwitch = true
+      } else if (this.foldSwitch === false) {
+        this.foldSwitch = true
+        this.unfoldSwitch = false
+      }
+    }
+  }
 }
-
 </script>
 <style lang='less' scoped>
 .left {
@@ -51,6 +72,10 @@ export default {
     border-radius: 100%;
     margin-right: 5px;
   }
+  i {
+    vertical-align: middle;
+    font-size: 20px;
+    margin-left: 10px;
+  }
 }
-
 </style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,14 +1,14 @@
 <template>
   <el-container>
+    <!-- 运用ElementUI的布局容器 -->
+    <!-- 左侧容器 -->
     <el-aside class="navigation" :style="leftStyle">
-      <!-- 左侧导航菜单 -->
-      <!-- <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
-        <el-radio-button :label="false">展开</el-radio-button>
-        <el-radio-button :label="true">收起</el-radio-button>
-      </el-radio-group>-->
+      <!-- logo图片 -->
       <div class="menu-title" style="background-color:#2e2f32">
+        <i class="el-icon-platform-eleme"></i>
         <img src="../../assets/img/logo_admin.png" alt />
       </div>
+      <!-- 导航菜单 -->
       <el-menu
         style="width:188px"
         class="navigation"
@@ -52,9 +52,9 @@
     </el-aside>
     <!-- 大容器 -->
     <el-container>
+      <!-- 头部容器 调用头部容器子组件 -->
       <el-header>
-        <el-button @click="abc">mmda</el-button>
-        <home-header></home-header>
+        <home-header @foldMetod="fold"></home-header>
       </el-header>
       <el-main>
         <!-- 二级路由容器 -->
@@ -69,24 +69,28 @@
 import homeHeader from '../../components/home-header'
 
 export default {
+  // 在实例中使用 components 属性注册需要用到的组件
   components: {
     'home-header': homeHeader
   },
   data () {
     return {
+      // 优化折叠  让左侧容器 width改变 这样主页所有容器才能改变
       leftStyle: {
         width: '189px',
         transition: 'all 0.6s'
       },
-      // 优化折叠
+      // 同上
       submenuStyle: {
         width: '189px'
       },
+      // 折叠开关
       isCollapse: false
     }
   },
   methods: {
-    abc () {
+    // 折叠方法  比较笨
+    fold () {
       if (this.isCollapse === false) {
         this.isCollapse = true
         this.leftStyle.width = '65px'
@@ -108,9 +112,16 @@ export default {
   .menu-title {
     padding: 8px 0;
     width: 187px;
-    text-align: center;
+    padding-left: 14px;
+    i {
+      font-size: 36px;
+      color: #ffffff;
+    }
     img {
+      // 尽量让他们中线对齐
+      vertical-align: top;
       height: 30px;
+      margin: 3px;
     }
   }
 }
