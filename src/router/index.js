6edd9a8dde615af9fb3d63924209d4e7919a44f2
hallelujah1019 <1106661258@qq.com>
2020-04-05 14:04:54 +0800

    设置路由导航守卫，控制页面访问权限

--- src/router/index.js
+++ src/router/index.js
@@ -47,4 +47,24 @@ const router = new VueRouter({
   routes
 })
 
+// 路由拦截器 beforeEach 方法 ,该方法接受一个函数作为参数
+//  参数1 to，表示去哪里的路由信息
+// 参数2 from，表示来自哪里的路由信息
+// 参数3 next，他是一个方法，用于路由方行
+// ------------- 判断用户登录状态，有就通过，没有就跳转到登录页 ------------
+router.beforeEach((to, from, next) => {
+// 如果访问的事登录页，直接方行
+  if (to.path === '/login') {
+    next()
+    return
+  }
+  // 如果不是登录页面 获取token
+  const token = window.localStorage.getItem('user_token')
+  if (token) {
+    next()
+  } else {
+    next('/login')
+  }
+})
+
 export default router
